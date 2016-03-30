a634b0940af0e6e94fda099a43d1c4c8f9b83027
Stefan Kok <stefan@prdct.nl>
2016-03-30 14:29:20 +0200

    Dockerfile is working

--- data_import/import.sh
+++ data_import/import.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-/usr/bin/python3.5 /import_data.py --user $DB_NAME \
-                                   --password $DB_PASSWORD \
-                                   --host $DATABASE_PORT_5432_TCP_ADDR \
-                                   --port $DATABASE_PORT_5432_TCP_PORT \
-                                   --database parkeervakken \
-                                   update \
-                                   --source /app/diva
+/usr/local/bin/python /import_data.py --user $DB_NAME \
+                                      --password $DB_PASSWORD \
+                                      --host $DATABASE_PORT_5432_TCP_ADDR \
+                                      --port $DATABASE_PORT_5432_TCP_PORT \
+                                      --database parkeervakken \
+                                      update \
+                                      --source /app/diva
