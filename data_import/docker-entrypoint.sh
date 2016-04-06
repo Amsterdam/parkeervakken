91f8af0f926a07bd267f91d4bf02c09d4747efc9
Stephan Preeker <stephan@preeker.net>
2016-04-06 10:36:06 +0200

    ATLAS-1492 add missing docker-entry point..

---
+++ data_import/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# wait for database to load
+source docker-wait.sh
+
+#unzip $(ls -Art data/*.zip | tail -n 1) -d /app/unzipped/
+
+
+# clear and or create tables
+python import_data.py --user $PARKEERVAKKEN_DB_USER \
+		      --password $PARKEERVAKKEN_DB_PASSWORD \
+		      --host $PARKEERVAKKEN_DB_PORT_5432_TCP_ADDR \
+		      --port $PARKEERVAKKEN_DB_PORT_5432_TCP_PORT \
+		      --database parkeervakken \
+                      initialize
+
+# run import / update data
+python import_data.py --user $PARKEERVAKKEN_DB_USER \
+		      --password $PARKEERVAKKEN_DB_PASSWORD \
+		      --host $PARKEERVAKKEN_DB_PORT_5432_TCP_ADDR \
+		      --port $PARKEERVAKKEN_DB_PORT_5432_TCP_PORT \
+		      --database parkeervakken \
+                      update \
+                      --source /app/unzipped
