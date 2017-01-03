9d29757265f2b6486f0cbf1bb79e5a981e4e6be1
Thijs Creemers <thijs.creemers@gmail.com>
2017-01-03 14:44:48 +0100

    #tg-518 set -e / set -u in all bash scripts

--- data_import/import.sh
+++ data_import/import.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -u
+set -e
+
 /usr/local/bin/python /import_data.py --user $DB_NAME \
                                       --password $DB_PASSWORD \
                                       --host $DATABASE_PORT_5432_TCP_ADDR \
